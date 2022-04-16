07ac7bae125e8bdb0347f3b3cc40fe25ce782ed5
evan <evan@purlcomputers.com>
2022-04-15 20:35:25 -0600

    Added presence line

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ bot = discord.Bot(intents=intents)
 
 dirr = sys.path[0]  # Main directory path
 
+gm = bot.create_group('gm', 'Gamemaster command prefix')
+
 
 def tokengrabber():
     if not os.path.exists(f"config/token.txt"):
@@ -28,6 +30,8 @@ def tokengrabber():
 async def on_ready():
     print("GameMaster 2.0 has been initialized.")
     print(f"We have logged in as {bot.user}")
+    print(f"Py-Cord version: {discord.__version__}")
+    await bot.change_presence(activity=discord.Game('Powered by NLS: https://www.nitelifesoftware.com'))
 
 
 @bot.event
@@ -37,23 +41,17 @@ async def on_guild_join(guild):
         os.mkdir(f"{dirr}/servers/{str(guild.id)}/Players")
 
 
-@bot.slash_command(guild_ids=Support, description="Command used to create a unit.")
+@gm.command(guild_ids=Support, description="Command used to create a unit.")
 # @commands.cooldown(rate=1, per=7200, type=commands.BucketType.member)
-@has_role("Game Master")
 async def createunit(ctx):  # Unit creation command
-    pass
-
-
-@createunit.error
-async def create_error(ctx, error):
-    if isinstance(error, commands.MissingRole):
-        msg = "You don't have the proper role! You require the role 'Game Master'!"
-        await ctx.send(msg)
+    role = discord.utils.get(ctx.guild.roles, name="Game Master")
+    if role in ctx.user.roles:
+        await ctx.respond("WIP")
     else:
-        print(error)
+        await ctx.respond("You do not have the proper role to run this command. Required role: **Game Master**")
 
 
-@bot.slash_command(guild_ids=Support, description="Command used to get the server's units.")
+@gm.command(guild_ids=Support, description="Command used to get the server's units.")
 async def units(ctx):
     os.chdir(dirr)
     un = []
@@ -63,12 +61,12 @@ async def units(ctx):
             num = 0
             await ctx.respond(f"**{ctx.guild.name}'s Units:**")
             for a, data in enumerate(filelist):
-                un.append(f"{a+1}: " + data.replace(".csv", ""))
+                un.append(f"{a + 1}: " + data.replace(".csv", ""))
                 if num == 15:
                     await ctx.send('\n'.join(un))
                     num = 0
                     un.clear()
-                if a == len(filelist) - 1: # if the last unit has come, and the list isn't empty, send the last units
+                if a == len(filelist) - 1:  # if the last unit has come, and the list isn't empty, send the last units
                     if len(un) != 0:
                         await ctx.send('\n'.join(un))
                 num += 1
@@ -77,21 +75,17 @@ async def units(ctx):
                 "No units are currently on the server, create some using the /createunit command!")
 
 
-@bot.slash_command(guild_ids=Support, description="Command used to create a unit, but quicker.")
+@gm.command(guild_ids=Support, description="Command used to create a unit, but quicker.")
 # @commands.cooldown(rate=1, per=7200, type=commands.BucketType.member)
-@has_role("Design Lead")
-async def quickcreate(ctx, name, shields, health, shielddamage, hulldamage, shieldregen):  # Unit creation command
-    await ctx.respond(
-        f"The unit you tried creating: {name} {shields} {health} {shielddamage} {hulldamage} {shieldregen}")
-
-
-@quickcreate.error
-async def quickcreate_error(ctx, error):
-    if isinstance(error, commands.MissingRole):
-        msg = "You don't have the proper role! You require admin privileges!"
-        await ctx.send(msg)
+async def quickcreate(ctx, name, shields, health, sdamage, damage, shieldregen):  # Unit creation command
+    role = discord.utils.get(ctx.guild.roles, name="Game Master")
+    if role in ctx.user.roles:
+        with open(f"{dirr}/Stats/{str(ctx.guild.id)}/{name}.csv", "w+") as csv_file:
+            csv_file.write(
+                f"{name}\nHealth: {health}\nShield: {shields}\nShield Regen: {shieldregen}\nShield Damage: {sdamage}\nDamage: {damage}")
+        await ctx.respond(f"Unit with the name {name} has been created.")
     else:
-        print(error)
+        await ctx.respond("You do not have the proper role to create units. Required role: Game Master")
 
 
 bot.run(tokengrabber())
